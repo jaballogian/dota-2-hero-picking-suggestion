efe35a2f8d58203f58e4e718a3be46fe5d3b3c06
jaballogian <jaballogianrma77@gmail.com>
2021-04-09 17:39:32 +0700

    commented App Context Provider component in root index file

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css'  ;
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {AppContextProvider} from './contexts/AppContext'
+// import {AppContextProvider} from './contexts/AppContext'
 
 ReactDOM.render(
-  <AppContextProvider>
+  // <AppContextProvider>
     <App />
-  </AppContextProvider>
+  // </AppContextProvider>
   ,
   document.getElementById('root')
 );
