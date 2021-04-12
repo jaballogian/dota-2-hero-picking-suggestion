c558b92d8882e498bac03ff8b5376179f2fe5b15
jaballogian <jaballogianrma77@gmail.com>
2021-04-12 12:56:56 +0700

    added is hovered state in Signle Hero Image and showed the icons if is hovered state is true

--- src/components/singleHeroImage/SingleHeroImage.js
+++ src/components/singleHeroImage/SingleHeroImage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 // IMAGES
 import heroCoverImageList from '../../data/heroes/heroCoverImageList'
@@ -7,21 +7,39 @@ import heroCoverImageList from '../../data/heroes/heroCoverImageList'
 import AddCircleOutlineRoundedIcon from '@material-ui/icons/AddCircleOutlineRounded'
 import BlockRoundedIcon from '@material-ui/icons/BlockRounded'
 
-function SingleHeroImage(){  
+function SingleHeroImage(){
+    // DEFINING CONSTANT VARIABLES
+    const [isHovered, setIsHovered] = useState(false)
+
+    const addHeroToOpponentsTeam =
+        isHovered &&
+        <AddCircleOutlineRoundedIcon style={{color: 'crimson', height: 40, width: 40, position: 'absolute', top: 5, left: 5, cursor: 'pointer'}}/>
+
+    const addHeroToYourTeam =
+        isHovered &&
+        <AddCircleOutlineRoundedIcon style={{color: 'forestgreen', height: 40, width: 40, position: 'absolute', top: 5, right: 5, cursor: 'pointer'}}/>
+    
+    const yourTeamBansThisHero =
+        isHovered &&
+        <BlockRoundedIcon style={{color: 'forestgreen', height: 40, width: 40, position: 'absolute', bottom: 5, right: 5, cursor: 'pointer'}}/>
+    
+    const yourOpponentsTeamBansThisHero =
+        isHovered &&
+        <BlockRoundedIcon style={{color: 'crimson', height: 40, width: 40, position: 'absolute', bottom: 5, left: 5, cursor: 'pointer'}}/>
+
+    // RETURNING VIEW
     return(
-        <div style={{position: 'absolute', width: 256, height: 144}}>
+        <div 
+            style={{position: 'absolute', width: 256, height: 144}} 
+            onMouseEnter={() => setIsHovered(true)} 
+            onMouseLeave={() => setIsHovered(false)}
+        >
             <img src={heroCoverImageList[0].default} alt='' style={{position: 'absolute', width: '100%', height: '100%'}}/>
-            {/* ADD HERO TO OPPONENT'S TEAM */}
-            <AddCircleOutlineRoundedIcon style={{color: 'crimson', height: 40, width: 40, position: 'absolute', top: 5, left: 5, cursor: 'pointer'}}/>
-
-            {/* ADD HERO TO YOUR TEAM */}
-            <AddCircleOutlineRoundedIcon style={{color: 'forestgreen', height: 40, width: 40, position: 'absolute', top: 5, right: 5, cursor: 'pointer'}}/>
-
-            {/* YOUR TEAM BAN THIS HERO */}
-            <BlockRoundedIcon style={{color: 'forestgreen', height: 40, width: 40, position: 'absolute', bottom: 5, right: 5, cursor: 'pointer'}}/>
-
-            {/* YOUR OPPONENT'S TEAM BANS THIS HERO */}
-            <BlockRoundedIcon style={{color: 'crimson', height: 40, width: 40, position: 'absolute', bottom: 5, left: 5, cursor: 'pointer'}}/>
+            
+            {addHeroToOpponentsTeam}
+            {addHeroToYourTeam}
+            {yourTeamBansThisHero}
+            {yourOpponentsTeamBansThisHero}
         </div>
     )
 }
