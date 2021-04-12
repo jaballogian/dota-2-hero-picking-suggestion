3b6d67f31f2c0ac7e27e09d8da99bebf5b0c0c7a
jaballogian <jaballogianrma77@gmail.com>
2021-04-12 10:53:29 +0700

    added a list of hero cover images

---
+++ src/data/heroes/heroCoverImageList.js
@@ -0,0 +1,7 @@
+function importAll(inputData) {
+    return inputData.keys().map(inputData);
+}
+
+const heroCoverImageList = importAll(require.context('../../images/hero-covers/', false, /\.(png|jpe?g|svg)$/))
+
+export default heroCoverImageList
