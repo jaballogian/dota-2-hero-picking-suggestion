b3302da7afff791a8009c0d91e2185076b89f4dd
jaballogian <jaballogianrma77@gmail.com>
2021-04-12 17:11:42 +0700

    added roles unit counters in Team Information Context file

--- src/contexts/TeamInformationContext.js
+++ src/contexts/TeamInformationContext.js
@@ -12,9 +12,20 @@ function TeamInformationContextProdiver(props){
     const summarizeTeamInformation = (inputHeroListTeam) => {
         let meleeCount = 0
         let rangeCount = 0
+
         let strengthCount = 0
         let agilityCount = 0
         let intelligenceCount = 0
+
+        let carryCount = 0
+        let supportCount = 0
+        let nukerCount = 0
+        let disablerCount = 0
+        let junglerCount = 0
+        let durableCount = 0
+        let escapeCount = 0
+        let pusherCount = 0
+        let initiatorCount = 0
 
         for(let i = 0; i < inputHeroListTeam.length; i++){
             const item = inputHeroListTeam[i]
@@ -37,8 +48,40 @@ function TeamInformationContextProdiver(props){
             else if(item['primary_attr'] === 2){
                 intelligenceCount++
             }
+
+            // COUNT FOR HERO ROLES
+            if(item['role_levels'][0] > 0){
+                carryCount++
+            }
+            else if(item['role_levels'][1] > 0){
+                supportCount++
+            }
+            else if(item['role_levels'][2] > 0){
+                nukerCount++
+            }
+            else if(item['role_levels'][3] > 0){
+                disablerCount++
+            }
+            else if(item['role_levels'][4] > 0){
+                junglerCount++
+            }
+            else if(item['role_levels'][5] > 0){
+                durableCount++
+            }
+            else if(item['role_levels'][6] > 0){
+                escapeCount++
+            }
+            else if(item['role_levels'][7] > 0){
+                pusherCount++
+            }
+            else if(item['role_levels'][8] > 0){
+                initiatorCount++
+            }
         }
-        return {meleeCount, rangeCount, strengthCount, agilityCount, intelligenceCount}
+        return {meleeCount, rangeCount,
+            strengthCount, agilityCount, intelligenceCount,
+            carryCount, supportCount, nukerCount, disablerCount, junglerCount, durableCount, escapeCount, pusherCount, initiatorCount
+        }
     }
 
     // CALLING CONSTANT VARIABLES
