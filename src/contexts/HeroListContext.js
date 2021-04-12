acf6bdeacce451d10a39d61ec4f642f08a8b2f3a
jaballogian <jaballogianrma77@gmail.com>
2021-04-12 15:02:46 +0700

    added addToMyHeroList, addToOpponentsHeroList, and addToBannedHeroList functions in Hero List Context file

--- src/contexts/HeroListContext.js
+++ src/contexts/HeroListContext.js
@@ -7,12 +7,36 @@ const HeroListContext = React.createContext()
 function HeroListContextProvider(props){
     // DEFINING CONSTANT VARIABLES
     const [heroPoolList, setHeroPoolList] = useState(sortedHeroDetailList)
+    const [myHeroList, setMyHeroList] = useState([])                        // MAXIMUM LENGTH IS 5
+    const [opponentsHeroList, setOpponentsHeroList] = useState([])          // MAXIMUM LENGTH IS 5
+    const [bannedHeroList, setBannedHeroList] = useState([])                // MAXIMUM LENGTH IS 14
 
+    // DEFINING CONSTANT FUNCTIONS
+    const addToMyHeroList = () => {
+        console.log('addToMyHeroList')
+    }
+
+    const addToOpponentsHeroList = () => {
+        console.log('addToOpponentsHeroList')
+    }
+
+    const addToBannedHeroList = () => {
+        console.log('addToBannedHeroList')
+    }
+
+    // CALLING CONSTANT FUNCTIONS
     // console.log('heroPoolList', heroPoolList)
 
     // RETURNING VIEW
     return(
-        <HeroListContext.Provider value={{heroPoolList}}>
+        <HeroListContext.Provider 
+            value={{
+                heroPoolList,
+                addToMyHeroList,
+                addToOpponentsHeroList,
+                addToBannedHeroList
+            }}
+        >
             {props.children}
         </HeroListContext.Provider>
     )
