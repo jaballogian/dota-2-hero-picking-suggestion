2121c2583894f1ad6312da6eafe7735320500cc3
jaballogian <jaballogianrma77@gmail.com>
2021-04-12 13:20:09 +0700

    added hero fullname to Single Hero Image

--- src/components/singleHeroImage/SingleHeroImage.js
+++ src/components/singleHeroImage/SingleHeroImage.js
@@ -2,10 +2,14 @@ import React, {useState} from 'react'
 
 // IMAGES
 import heroCoverImageList from '../../data/heroes/heroCoverImageList'
+import heroAttribute from '../../images/attributes/hero_agility.png'
 
 // ICONS
 import AddCircleOutlineRoundedIcon from '@material-ui/icons/AddCircleOutlineRounded'
 import BlockRoundedIcon from '@material-ui/icons/BlockRounded'
+
+// MATERIAL UI LIBRARIRES
+import Typography from '@material-ui/core/Typography';
 
 function SingleHeroImage(){
     // DEFINING CONSTANT VARIABLES
@@ -31,6 +35,32 @@ function SingleHeroImage(){
             <BlockRoundedIcon style={{color: 'grey', height: 30, width: 30, cursor: 'pointer'}}/>
             <AddCircleOutlineRoundedIcon style={{color: 'forestgreen', height: 30, width: 30, cursor: 'pointer'}}/>
         </div>
+
+    const heroFullname = 
+        isHovered &&
+        <div 
+            style={{
+                width: '100%', 
+                height: 50,
+                position: 'absolute', 
+                left: 0, 
+                bottom: 0, 
+                display: 'flex', 
+                alignItems: 'center',
+                padding: '0px 5px 5px 5px', 
+                boxSizing: 'border-box'
+            }}
+            >
+            <img src={heroAttribute} alt='' style={{height: 30, width: 30, marginRight: 10}}/>
+            <Typography 
+                variant="h6" 
+                style={{
+                    color: 'white'
+                }}
+            >
+                HERO FULLNAME
+            </Typography>
+        </div>
     
     // RETURNING VIEW
     return(
@@ -42,6 +72,7 @@ function SingleHeroImage(){
             <img src={heroCoverImageList[0].default} alt='' style={{position: 'absolute', width: '100%', height: '100%'}}/>
             
             {actionOptions}
+            {heroFullname}
         </div>
     )
 }
