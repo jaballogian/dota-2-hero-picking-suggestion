0ea88e46afb7912d07d264cad5ceea73e1b6ac6f
jaballogian <jaballogianrma77@gmail.com>
2021-04-13 13:48:37 +0700

    simplified attribute type views in Team Information component

--- src/components/teamInformation/TeamInformation.js
+++ src/components/teamInformation/TeamInformation.js
@@ -15,6 +15,7 @@ import Typography from '@material-ui/core/Typography';
 function TeamInformation(props){
     // DEFINING CONSTANT VARIABLES
     const attackTypeIconList = [MeleeIcon, RangedIcon]
+    const attributeTypeIconList = [StrengthAttribute, AgilityAttribute, IntelligenceAttribute]
     const roleCountKeyList = [
         'Carry', 'Support', 'Nuker',
         'Disabler', 'Jungler', 'Durable',
@@ -24,6 +25,15 @@ function TeamInformation(props){
     const attackTypeListView = props.data.attackType.map((item, index) => (
         <div key={index} style={{display: 'flex'}}>
             <img src={attackTypeIconList[index]} alt='' style={{height: 20, width: 20, marginRight: 10}}/>
+            <Typography variant="h6" style={{color: 'black'}}>
+                {item}
+            </Typography>
+        </div>
+    ))
+
+    const attributeTypeListView = props.data.attributeType.map((item, index) => (
+        <div style={{display: 'flex'}}>
+            <img src={attributeTypeIconList[index]} alt='' style={{height: 30, width: 30, marginRight: 10}}/>
             <Typography variant="h6" style={{color: 'black'}}>
                 {item}
             </Typography>
@@ -54,29 +64,7 @@ function TeamInformation(props){
                 Attribute Types
             </Typography>
 
-            {/* STRENGTH COUNT */}
-            <div style={{display: 'flex'}}>
-                <img src={StrengthAttribute} alt='' style={{height: 30, width: 30, marginRight: 10}}/>
-                <Typography variant="h6" style={{color: 'black'}}>
-                    {props.data['attributeType']['strengthCount']}
-                </Typography>
-            </div>
-
-            {/* AGILITY COUNT */}
-            <div style={{display: 'flex'}}>
-                <img src={AgilityAttribute} alt='' style={{height: 30, width: 30, marginRight: 10}}/>
-                <Typography variant="h6" style={{color: 'black'}}>
-                    {props.data['attributeType']['agilityCount']}
-                </Typography>
-            </div>
-
-            {/* INTELLIGENCE COUNT */}
-            <div style={{display: 'flex'}}>
-                <img src={IntelligenceAttribute} alt='' style={{height: 30, width: 30, marginRight: 10}}/>
-                <Typography variant="h6" style={{color: 'black'}}>
-                    {props.data['attributeType']['intelligenceCount']}
-                </Typography>
-            </div>
+            {attributeTypeListView}
 
             {/* ATTRIBUTE TYPES */}
             <Typography variant="h6" style={{color: 'black'}}>
