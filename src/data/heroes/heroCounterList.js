048542a762154ec7ace26762464cc33c30b9160d
jaballogian <jaballogianrma77@gmail.com>
2021-04-17 22:39:46 +0700

    added Bounty Hunter to Crystal Maiden data to hero counter list file

--- src/data/heroes/heroCounterList.js
+++ src/data/heroes/heroCounterList.js
@@ -384,6 +384,374 @@ const heroCounterList = [
             "Warlock",
             "Zeus"
         ]
+    },
+    {
+        "id": 62,
+        "name_loc": "Bounty Hunter",
+        "bad_against": [
+            "Bloodseeker",
+            "Death Prophet",
+            "Luna",
+            "Rubick",
+            "Slardar",
+            "Spectre",
+            "Spirit Breaker",
+            "Venomancer",
+            "Viper",
+            "Zeus"
+        ],
+        "good_against": [
+            "Sniper",
+            "Nyx Assassin",
+            "Shadow Fiend",
+            "Pudge"
+        ],
+        "works_well_with": [
+            "Invoker",
+            "Nature's Prophet",
+            "Tinker",
+            "Spectre",
+            "Zeus"
+        ]
+    },
+    {
+        "id": 78,
+        "name_loc": "Brewmaster",
+        "bad_against": [
+            "Bane",
+            "Death Prophet",
+            "Lion",
+            "Outworld Destroyer",
+            "Shadow Shaman",
+            "Skywrath Mage"
+        ],
+        "good_against": [
+            "Abaddon",
+            "Chaos Knight",
+            "Huskar",
+            "Lone Druid",
+            "Lycan",
+            "Templar Assassin",
+            "Ursa"
+        ],
+        "works_well_with": [
+            "Dazzle",
+            "Oracle",
+            "Tiny",
+            "Invoker"
+        ]
+    },
+    {
+        "id": 99,
+        "name_loc": "Bristleback",
+        "bad_against": [
+            "Anti-Mage",
+            "Doom",
+            "Faceless Void",
+            "Grimstroke",
+            "Hoodwink",
+            "Legion Commander",
+            "Lich",
+            "Lion",
+            "Necrophos",
+            "Outworld Destroyer",
+            "Razor",
+            "Silencer",
+            "Slardar",
+            "Timbersaw",
+            "Viper",
+            "Weaver"
+        ],
+        "good_against": [
+            "Arc Warden",
+            "Broodmother",
+            "Chaos Knight",
+            "Huskar",
+            "Jakiro",
+            "Luna",
+            "Lycan",
+            "Meepo",
+            "Phantom Lancer",
+            "Pudge",
+            "Riki",
+            "Rubick",
+            "Sand King",
+        ],
+        "works_well_with": [
+            "Crystal Maiden",
+            "Dazzle",
+            "Drow Ranger",
+            "Keeper of the Light",
+            "Skywrath Mage",
+            "Venomancer"
+        ]
+    },
+    {
+        "id": 61,
+        "name_loc": "Broodmother",
+        "bad_against": [
+            "Alchemist",
+            "Axe",
+            "Ancient Apparition",
+            "Bristleback",
+            "Dark Seer",
+            "Dragon Knight",
+            "Earthshaker",
+            "Elder Titan",
+            "Ember Spirit",
+            "Kunkka",
+            "Legion Commander",
+            "Lina",
+            "Meepo",
+            "Necrophos",
+            "Pangolier",
+            "Sand King",
+            "Shadow Fiend",
+            "Storm Spirit",
+            "Sven",
+            "Tidehunter",
+            "Timbersaw",
+            "Void Spirit"
+        ],
+        "good_against": [
+            "Chen",
+            "Drow Ranger",
+            "Enchantress",
+            "Medusa",
+            "Nature's Prophet",
+            "Phantom Assassin",
+            "Sniper",
+            "Treant Protector"
+        ],
+        "works_well_with": [
+            "Beastmaster",
+            "Dazzle",
+            "Lifestealer"
+        ]
+    },
+    {
+        "id": 96,
+        "name_loc": "Centaur Warrunner",
+        "bad_against": [
+            "Clockwerk",
+            "Disruptor",
+            "Kunkka",
+            "Lifestealer",
+            "Necrophos",
+            "Nyx Assassin",
+            "Templar Assassin"
+        ],
+        "good_against": [
+            "Arc Warden",
+            "Doom",
+            "Nyx Assassin",
+            "Phantom Assassin",
+            "Phantom Lancer",
+            "Sniper",
+            "Windranger"
+        ],
+        "works_well_with": [
+            "Abaddon",
+            "Dark Seer",
+            "Mirana",
+            "Sven",
+            "Tusk",
+            "Tiny",
+            "Vengeful Spirit",
+            "Grimstroke"
+        ]
+    },
+    {
+        "id": 81,
+        "name_loc": "Chaos Knight",
+        "bad_against": [
+            "Anti-Mage",
+            "Abaddon",
+            "Tinker",
+            "Axe",
+            "Lion",
+            "Brewmaster",
+            "Dragon Knight",
+            "Earthshaker",
+            "Enigma",
+            "Jakiro",
+            "Lich",
+            "Magnus",
+            "Monkey King",
+            "Phantom Lancer",
+            "Omniknight",
+            "Pugna",
+            "Sand King",
+            "Sven",
+            "Tidehunter",
+            "Timbersaw",
+            "Winter Wyvern",
+            "Witch Doctor",
+            "Puck"
+        ],
+        "good_against": [
+            "Bane",
+            "Bounty Hunter",
+            "Disruptor",
+            "Lone Druid",
+            "Pudge",
+            "Rubick",
+            "Silencer",
+            "Slark",
+            "Techies",
+            "Ursa",
+            "Viper"
+        ],
+        "works_well_with": [
+            "Crystal Maiden",
+            "Keeper of the Light",
+            "Morphling",
+            "Shadow Demon"
+        ]
+    },
+    {
+        "id": 66,
+        "name_loc": "Chen",
+        "bad_against": [
+            "Crystal Maiden",
+            "Enchantress",
+            "Earthshaker",
+            "Legion Commander",
+            "Sven",
+            "Lina",
+            "Luna",
+            "Monkey King",
+            "Venomancer",
+            "Jakiro",
+            "Sand King",
+            "Timbersaw",
+            "Necrophos",
+            "Death Prophet",
+            "Dark Seer",
+            "Broodmother",
+            "Alchemist",
+            "Razor",
+            "Phoenix",
+            "Elder Titan",
+            "Batrider",
+            "Ancient Apparition",
+            "Underlord"
+        ],
+        "good_against": [
+            "Techies",
+            "Arc Warden",
+            "Beastmaster",
+            "Clockwerk",
+            "Pudge",
+            "Bane",
+            "Shadow Shaman"
+        ],
+        "works_well_with": [
+            "Vengeful Spirit",
+            "Lycan"
+        ]
+    },
+    {
+        "id": 56,
+        "name_loc": "Clinkz",
+        "bad_against": [
+            "Bloodseeker",
+            "Bounty Hunter",
+            "Doom",
+            "Enchantress",
+            "Lion",
+            "Morphling",
+            "Slardar",
+            "Spectre",
+            "Spirit Breaker",
+            "Treant Protector",
+            "Winter Wyvern",
+            "Zeus"
+        ],
+        "good_against": [
+            "Anti-Mage",
+            "Ember Spirit",
+            "Huskar",
+            "Nature's Prophet",
+            "Viper"
+        ],
+        "works_well_with": [
+            "Drow Ranger",
+            "Omniknight",
+            "Vengeful Spirit"
+        ]
+    },
+    {
+        "id": 51,
+        "name_loc": "Clockwerk",
+        "bad_against": [
+            "Anti-Mage",
+            "Chen",
+            "Clinkz",
+            "Dazzle",
+            "Disruptor",
+            "Ember Spirit",
+            "Huskar",
+            "Juggernaut",
+            "Lifestealer",
+            "Morphling",
+            "Omniknight",
+            "Phantom Lancer",
+            "Phoenix",
+            "Queen of Pain",
+            "Terrorblade",
+            "Ursa",
+            "Visage"
+        ],
+        "good_against": [
+            "Earthshaker",
+            "Enigma",
+            "Drow Ranger",
+            "Legion Commander",
+            "Monkey King",
+            "Pudge",
+            "Pugna",
+            "Sniper",
+            "Spirit Breaker",
+            "Tinker",
+            "Windranger"
+        ],
+        "works_well_with": [
+            "Dark Seer",
+            "Invoker",
+            "Lifestealer",
+            "Mirana",
+            "Shadow Demon"
+        ]
+    },
+    {
+        "id": 5,
+        "name_loc": "Crystal Maiden",
+        "bad_against": [
+            "Bounty Hunter",
+            "Bristleback",
+            "Clockwerk",
+            "Doom",
+            "Earth Spirit",
+            "Jakiro",
+            "Legion Commander",
+            "Luna",
+            "Naga Siren",
+            "Nyx Assassin",
+            "Tidehunter",
+            "Tusk",
+            "Visage"
+        ],
+        "good_against": [
+            "Slark",
+            "Phantom Assassin",
+            "Templar Assassin"
+        ],
+        "works_well_with": [
+            "Juggernaut",
+            "Timbersaw",
+            "Sven"
+        ]
     }
 ]
 
