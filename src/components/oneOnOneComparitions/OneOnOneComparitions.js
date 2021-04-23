e9c82a3e4edfe9f8f90952c55b5941e38550d149
jaballogian <jaballogianrma77@gmail.com>
2021-04-23 20:47:00 +0700

    added opponents row and displayed it in One On One Comparition component

--- src/components/oneOnOneComparitions/OneOnOneComparitions.js
+++ src/components/oneOnOneComparitions/OneOnOneComparitions.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
+
+// MATERIAL UI LIBRARIRES
+import Typography from '@material-ui/core/Typography';
+
+// CUSTOM COMPONENTS AND DATA
+import {HeroListContext} from '../../contexts/HeroListContext'
 
 function OneOnOneComparitions(props) {
     // DEFINING CONSTANT VARIABLES
+    const {myHeroList, opponentsHeroList} = useContext(HeroListContext)
+
+    const opponentsRow = opponentsHeroList.map((item, index) => (
+        <div key={index} style={{padding: 10, border: '1px solid black', backgroundColor: 'red'}}>
+            <img src={item.image} alt='' style={{height: 90, width: 160}}/>
+        </div>
+    ))
+    
     const oneOnOneMatrix = props.data.map((array, i) => {
         return(
             <div key={i} style={{display: 'flex', backgroundColor: 'orange'}}>
@@ -21,6 +35,20 @@ function OneOnOneComparitions(props) {
     // RETURNING VIEW
     return(
         <div >
+            {/* OPPONENT'S HERO LIST */}
+            <Typography 
+                variant="h6" 
+                style={{
+                    color: 'white'
+                }}
+            >
+                Opponent's Heroes
+            </Typography>
+            <div style={{display: 'flex', marginBottom: 10}}>
+                {opponentsRow}
+            </div>
+
+            {/* COMPARITION MATRIX */}
             {oneOnOneMatrix}
         </div>
     )
